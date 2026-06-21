221d7e1840c28b8a3e3e05d2cb0c1bcbe2fd7fa6
Yoon Young Eun <dms90904@gmail.com>
2026-06-21 17:21:59 +0900

    fix: 공휴일 수정

--- src/main/java/com/ssafy/lancit/domain/holiday/dto/HolidayDTO.java
+++ src/main/java/com/ssafy/lancit/domain/holiday/dto/HolidayDTO.java
@@ -1,5 +1,8 @@
 package com.ssafy.lancit.domain.holiday.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonSetter;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,5 +17,16 @@ public class HolidayDTO {
     private LocalDate date;       // 공휴일 날짜
     private String name;          // 공휴일 명칭 (신정, 설날 등)
     private int year;             // 연도
-    private boolean isHoliday;   // true = 공휴일, false = 대체공휴일
+    private boolean isHoliday;    // true = 공휴일, false = 대체공휴일
+
+    @JsonGetter("isHoliday")
+    public boolean isHoliday() {
+        return isHoliday;
+    }
+
+    @JsonSetter("isHoliday")
+    @JsonAlias("holiday")
+    public void setHoliday(boolean isHoliday) {
+        this.isHoliday = isHoliday;
+    }
 }
